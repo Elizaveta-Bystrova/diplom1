75afdc5ee6cdb9594064ecfb5d412863cdb21f53
Elizaveta-Bystrova <107638882+Elizaveta-Bystrova@users.noreply.github.com>
2024-04-05 13:16:46 +0300

    1videofinal

--- dispatcher/src/main/java/com/ivpek/DispatcerApplication.java
+++ dispatcher/src/main/java/com/ivpek/DispatcerApplication.java
@@ -10,5 +10,4 @@ class DispatcherApplication {
         SpringApplication.run(DispatcherApplication.class);
     }
 }
-public class DispatcerApplication {
-}
+
--- dispatcher/src/main/java/com/ivpek/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/ivpek/controller/TelegramBot.java
@@ -1,18 +1,23 @@
 package com.ivpek.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
     @Override
     public String getBotUsername() {
-        return "diplomivpek_bot";
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return "6804102621:AAGu-A8gyCkjP-ye_zYCXIheLLcacpaDq_Y";
+        return botToken;
     }
 
     @Override
