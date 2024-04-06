74b1b78b23b61e37ef6b70f36c9a4148bfff42e9
Elizaveta-Bystrova <107638882+Elizaveta-Bystrova@users.noreply.github.com>
2024-04-06 16:18:51 +0300

    1videofinal

--- dispatcher/src/main/java/com/ivpek/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/ivpek/controller/TelegramBot.java
@@ -1,15 +1,21 @@
 package com.ivpek.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 @Component
+@Log4j
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.name}")
     private String botName;
     @Value("${bot.token}")
     private String botToken;
+
     @Override
     public String getBotUsername() {
         return botName;
@@ -23,7 +29,22 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         var originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
+        log.debug(originalMessage.getText());
 
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hi");
+        sendAnswerMessage(response);
+
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
